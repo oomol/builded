d34b60075faf83b6d74f98866620c69b6c424269
ihexon <14349453+ihexon@users.noreply.github.com>
2024-12-09 06:42:31 +0000

    fix(caller): caller stdout/stderr same as ffmpeg's stdout/stderr

--- homebrew_packer/caller.sh
+++ homebrew_packer/caller.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -o pipefail
 
-SOURCE=${BASH_SOURCE[0]}
-while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+get_source_dir() {
+	SOURCE=${BASH_SOURCE[0]}
+	while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+		DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+		SOURCE=$(readlink "$SOURCE")
+		[[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	done
 	DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
-	SOURCE=$(readlink "$SOURCE")
-	[[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
 
-SCRIPT_DIR="${DIR}"
+	SCRIPT_DIR="${DIR}"
 
-cd "$SCRIPT_DIR" || {
-	echo "Error: failed to change the workdir to ${SCRIPT_DIR}"
-	exit 100
+	cd "$SCRIPT_DIR" || {
+		echo "Error: failed to change the workdir to ${SCRIPT_DIR}"
+		exit 100
+	}
+	export SCRIPT_DIR
 }
 
 cover_path() {
@@ -21,52 +24,56 @@ cover_path() {
 	./tools/cover_path "$@"
 }
 
+
+# log() echo all message into stderr
+log(){
+	echo "$@" 1>&2
+}
+
+
 ################### In Linux VM ###################
 called_in_guest() {
-	echo "==== Called in Linux Guest ===="
+	log "==== Called in Linux Guest ===="
 	if [[ -z "$*" ]]; then
-		echo "Error: args empty, what binary do you want call ?"
+		log "Error: args empty, what binary do you want call ?"
 		exit 100
 	fi
 
-	echo "CALLING WITH ARGS: $*"
+	log "CALLING WITH ARGS: $*"
 	# First we need chmod +x to caller.sh in MacOS HOST
 	SH_CALLER="${SCRIPT_DIR}/caller.sh"
-	set -xe
+	export SH_CALLER
+
+	if [[ ! -f /usr/bin/exec_macos ]];then
+		log "exec_macos not find in /usr/bin/"
+		exit 100
+	fi
+
 	exec_macos chmod +x "$SH_CALLER"
 	exec_macos "$SH_CALLER" "$@"
-	set +xe
 }
 
 ################### In MACOS HOST ###################
 called_in_host() {
-	echo "==== Called in MacOS Host ===="
+	log "==== Called in MacOS Host ===="
 	if [[ -z "$*" ]]; then
-		echo "Error: args empty, what binary do you want call ?"
+		log "Error: args empty, what binary do you want call ?"
 		exit 100
 	fi
-
-	set -ex
-
+	
 	export DYLD_LIBRARY_PATH="${SCRIPT_DIR}/lib"
 	export PATH="$SCRIPT_DIR/bin:$PATH"
 	chmod -R +x $SCRIPT_DIR/opt/homebrew/Cellar/ffmpeg
 
-	set +ex
-
-	cmdline=$(cover_path "$@"| grep NEW_CMELINE| cut -d ':' -f2-)
-	set -xe
+	cmdline=$(cover_path "$@" | grep NEW_CMELINE | cut -d ':' -f2-)
 	$cmdline
-	set +xe
 }
 
-main() {
-	OS=$(uname -s)
-	if [[ $OS == Linux ]]; then
-		called_in_guest "$@"
-	elif [[ $OS == Darwin ]]; then
-		called_in_host "$@"
-	fi
-}
+get_source_dir
 
-main "$@"  2>&1 | tee -a "$SCRIPT_DIR/exec.logs"
+if [[ $(uname -s) == Linux ]]; then
+	# stdout and stderr all into stderr
+	called_in_guest "$@"
+elif [[ $(uname -s) == Darwin ]]; then
+	called_in_host "$@"
+fi
